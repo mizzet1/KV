b16086d8fc294b0e7e520654576b819577ce3c5e
mizzet1 <riccardo.accts@gmail.com>
2025-04-22 10:23:57 +0200

    dashboard graph fixed

--- kv_store/lib/kv_store_web/telemetry.ex
+++ kv_store/lib/kv_store_web/telemetry.ex
@@ -1,14 +1,26 @@
 defmodule KvStoreWeb.Telemetry do
+  use Supervisor
   import Telemetry.Metrics
+
+  def start_link(_arg) do
+    Supervisor.start_link(__MODULE__, [])
+  end
+
+  @impl true
+  def init(_) do
+    children = []
+
+    Supervisor.init(children, strategy: :one_for_one)
+  end
 
   def metrics do
     [
-      summary("kv store calls latency",
+      summary("kv_store.operation.latency",
         unit: {:native, :millisecond},
         tags: [:operation],
         description: "Time taken for KV store operations (get, put, delete)"
       ),
-      counter("kv store calls count",
+      counter("kv_store.operation.count",
         tags: [:operation],
         description: "Total number of KV store operations"
       )
